ccef5cbb50a2dd52e386227f16ae91dc174bd6fc
RaffaeleCarrieri <raffaelecarrieridev@gmail.com>
2024-11-29 00:28:54 +0100

    configuration profiles

--- src/backoffice.module.ts
+++ src/backoffice.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppModelProvider } from './app/model/AppModelRepository';
 import { AppModel, AppModelSchema } from './app/model/AppModel';
 import { AppModelController } from './app/AppModelController';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
@@ -15,13 +16,18 @@ import { AppModelController } from './app/AppModelController';
           schema: AppModelSchema
         }
       ]
-    )
+    ),
+    ConfigModule.forRoot({
+      envFilePath: ['.env',`.${process.env.NODE_ENV || 'test'}.env`],
+      isGlobal: true, // Rende le variabili disponibili in tutta l'app
+    })
   ],
   controllers: [AppModelController],
   providers: [AppModelProvider],
   //exports:[AppModelProvider]
 
 })
+
 
 export class BackOfficeModule {
 
